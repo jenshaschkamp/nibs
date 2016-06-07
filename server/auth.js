4c262080fedc4f072288733d930b28d550315d9e
jenshaschkamp <jens.haschkamp@gmail.com>
2016-06-07 21:40:29 +0200

    Update auth.js

--- server/auth.js
+++ server/auth.js
@@ -80,7 +80,7 @@ function login(req, res, next) {
         return res.send(401, invalidCredentials);
     }
 
-    db.query('SELECT id, firstName, lastName, email, JJH__loyaltyid__c as externalUserId, JJH__password__c AS password FROM salesforce.contact WHERE email=$1', [creds.email], true)
+    db.query('SELECT id, firstName, lastName, email, loyaltyid__c as externalUserId, password__c AS password FROM salesforce.contact WHERE email=$1', [creds.email], true)
         .then(function (user) {
             if (!user) {
                 return res.send(401, invalidCredentials);
@@ -176,7 +176,7 @@ function createUser(user, password) {
     var deferred = Q.defer(),
         externalUserId = (+new Date()).toString(36); // TODO: more robust UID logic
 
-    db.query('INSERT INTO salesforce.contact (email, JJH__password__c, firstname, lastname, leadsource, JJH__loyaltyid__c, accountid) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id, firstName, lastName, email, loyaltyid__c as externalUserId',
+    db.query('INSERT INTO salesforce.contact (email, password__c, firstname, lastname, leadsource, loyaltyid__c, accountid) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id, firstName, lastName, email, loyaltyid__c as externalUserId',
         [user.email, password, user.firstName, user.lastName, 'Loyalty App', externalUserId, config.contactsAccountId], true)
         .then(function (insertedUser) {
             deferred.resolve(insertedUser);
