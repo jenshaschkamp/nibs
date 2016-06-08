1b5f9aafa0b914c8f3815431fd7d0adda069caaf
jenshaschkamp <jens.haschkamp@gmail.com>
2016-06-08 14:25:51 +0200

    Update config.js

--- client/js/config.js
+++ client/js/config.js
@@ -2,7 +2,7 @@ angular.module('nibs.config', [])
 
     .constant('SERVER_URL', null)
 
-    .constant('FB_APP_ID','1618878675020266')
+    .constant('FB_APP_ID','1627928404195330')
 
     .constant('STATUS_LABELS', [
         'Forastero',
